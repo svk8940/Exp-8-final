a4c2a285535a5e92d66d2b4b05d6aaa6371d984e
Shubham Kamble <svk8940@gmail.com>
2025-11-24 23:49:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def add():
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8083, debug=True)
+    app.run(host="0.0.0.0", port=8083, debug=True)
